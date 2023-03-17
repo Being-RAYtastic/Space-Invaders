dff555bec378b366f95193e50a2d77eb387ba1bb
Being-RAYtastic <risheet.ray@gmail.com>
2023-03-17 23:18:03 +0530

    fix-width of canvas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 1024
+canvas.height = 576
 
 
 const player = new Player()
